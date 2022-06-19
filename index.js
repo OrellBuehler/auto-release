000ac0734a591cb7ead041b29a156e96b0913ebf
Orell Bühler <17276101+OrellBuehler@users.noreply.github.com>
2022-06-19 18:20:09 +0200

    feat: add version script to get new tag name

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
+const version = require("./version");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const majorBranch = core.getInput("major-branch");
+    const minorBranch = core.getInput("minor-branch");
+    const patchBranch = core.getInput("patch-branch");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    const tagName = await version(github.ref_name, {
+      majorBranch: majorBranch,
+      minorBranch: minorBranch,
+      patchBranch: patchBranch,
+    });
+
+    core.debug(`Tag name: ${tagName}`);
+
+    core.info(new Date().toTimeString());
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
---
+++ version.js
@@ -0,0 +1,42 @@
+const github = require("@actions/github");
+
+module.exports = async (target, { majorBranch, minorBranch, patchBranch }) => {
+  const query = `query ($owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name) {
+            refs(refPrefix: "refs/tags/", last: 1) {
+            nodes {name}
+            }
+        }
+    }`;
+
+  const result = await github.graphql(query, {
+    owner: github.context.repo.owner,
+    name: github.context.repo.repo,
+  });
+
+  const latestTag = result.repository.refs.nodes[0].name;
+  const versionPart = latestTag.substring(1);
+  const versionNumbers = versionPart.split(".");
+  var major = versionNumbers[0];
+  var minor = versionNumbers[1];
+  var patch = versionNumbers[2];
+
+  switch (target) {
+    case majorBranch:
+      major++;
+      minor = 0;
+      patch = 0;
+      break;
+    case minorBranch:
+      minor++;
+      patch = 0;
+      break;
+    case patchBranch:
+      patch++;
+      break;
+    default:
+      throw new Error(`Unknown target branch: ${target}`);
+  }
+
+  return `v${major}.${minor}.${patch}`;
+};
